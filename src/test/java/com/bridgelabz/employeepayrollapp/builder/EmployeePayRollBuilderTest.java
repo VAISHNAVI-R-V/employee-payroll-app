436cb3f4262257c26766d286d57d549400bbeb6b
VAISHNAVI-R-V <vaishucv14@gmail.com>
2021-12-16 14:01:00 +0530

    ADDED : Test case  for EMPLOYEE BUILDER class to test converting of employee-DTO to employee-ENTITY using MODEL-MAPPER in employee payroll.

---
+++ src/test/java/com/bridgelabz/employeepayrollapp/builder/EmployeePayRollBuilderTest.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.employeepayrollapp.builder;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDto;
+import com.bridgelabz.employeepayrollapp.entity.EmployeePayrollEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeePayRollBuilderTest {
+    @InjectMocks
+    private EmployeePayRollBuilder employeePayRollBuilder;
+    private ModelMapper modelMapper;
+
+    @Test
+    void name() {
+        EmployeePayrollDto employeeDto = new EmployeePayrollDto();
+        employeeDto.setName("Damini");
+        employeeDto.setGender("Female");
+        employeeDto.setSalary("35000");
+        employeeDto.setDepartment("It");
+        employeeDto.setNotes("Welcome to company");
+        employeeDto.setImagePath("k.jpg");
+        employeeDto.setStartDate(new Date());
+
+        EmployeePayrollEntity employeeEntity = new EmployeePayrollEntity();
+        employeeEntity = employeePayRollBuilder.buildEmployeeEntity(employeeDto, employeeEntity);
+        assertEquals(employeeDto.getName(), employeeEntity.getName());
+
+    }
+}
